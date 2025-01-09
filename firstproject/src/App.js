2aef66348f020cc8b79bb628f125735ef9ffa550
Saya1206 <sayali.patil0831>
2025-01-09 17:01:28 +0530

    Add FourthComponent and update App.js

--- firstproject/src/App.js
+++ firstproject/src/App.js
@@ -18,12 +18,26 @@ function App() {
 export default App;
 */
 
+/*
 import ThirdComponent from "./components/ThirdComponent";
 
 function App() {
   return (
     <div>
       <ThirdComponent name="ajay" age="22" />
+    </div>
+  );
+}
+
+export default App;
+*/
+
+import FourthComponent from "./components/FourthComponent";
+
+function App() {
+  return (
+    <div>
+      <FourthComponent name="swara" age="12" />
     </div>
   );
 }
---
+++ firstproject/src/components/FourthComponent.js
@@ -0,0 +1,9 @@
+export default function FourthComponent({ name, age }) {
+  return (
+    <>
+      <h2>Fourth Component</h2>
+      <p>{name}</p>
+      <p>{age}</p>
+    </>
+  );
+}
