0f1f6ab494cd6ce4369ae7bacb4e0083f304a748
Saya1206 <sayali.patil0831>
2025-02-03 21:05:51 +0530

    Add Routes

--- eproduct-management/src/App.js
+++ eproduct-management/src/App.js
@@ -1,10 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./components/Home";
+import About from "./components/About";
+import Contact from "./components/Contact";
+import Product from "./components/Product";
+import DemoNavbar from "./components/DemoNavbar";
+
 
 function App() {
   return (
     <>
-    
+      <BrowserRouter>
+        <DemoNavbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/product" element={<Product />} />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
