0053f3adb1944a221d5e63139da3dd9a76357f79
Saya1206 <sayali.patil0831>
2025-02-03 21:47:23 +0530

    Add Home Page

---
+++ eproduct-management/src/components/Home.js
@@ -0,0 +1,14 @@
+import React from "react";
+import DemoJumbotron from "./DemoJumbotron";
+import Features from "./Features";
+import Footer from "./Footer";
+
+export default function Home() {
+  return (
+    <div>
+      <DemoJumbotron />
+      <Features />
+      <Footer />
+    </div>
+  );
+}
