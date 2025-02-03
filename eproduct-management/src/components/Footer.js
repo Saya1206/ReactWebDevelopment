41c3e27afbfb8a70970f9fae7f2930dc56502555
Saya1206 <sayali.patil0831>
2025-02-03 21:56:51 +0530

    Add Footer to Home Page

---
+++ eproduct-management/src/components/Footer.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Footer() {
+  return (
+    <div className="bg-secondary text-center text-white py-3">
+      <p>&copy; 2024 BrandName. All Rights Reserved.</p>
+      <span>Privacy Policy</span> |<span> Terms of Service</span>
+    </div>
+  );
+}
