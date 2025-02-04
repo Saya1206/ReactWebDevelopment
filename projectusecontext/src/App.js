0c88d237645c654f80068b48bf71b69bca426799
Saya1206 <sayali.patil0831>
2025-02-04 13:44:35 +0530

    Integrate UserContext provider in App.js

--- projectusecontext/src/App.js
+++ projectusecontext/src/App.js
@@ -1,5 +1,22 @@
-import "./App.css";
+/*
 import FirstComponent from "./components/FirstComponent";
+
+// for prop drilling
+
+function App() {
+  return (
+    <>
+      <FirstComponent />
+    </>
+  );
+}
+
+export default App;
+*/
+
+// for using context
+
+import FirstComponent from "./contextscomponents/FirstComponent";
 
 function App() {
   return (
