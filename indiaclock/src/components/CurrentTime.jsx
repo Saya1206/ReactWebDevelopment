3259defbfa917ec85c044b9129f2603b9350169a
Saya1206 <sayali.patil0831>
2025-05-02 21:51:56 +0530

    Update CurrentTime component to show live time using hooks

--- indiaclock/src/components/CurrentTime.jsx
+++ indiaclock/src/components/CurrentTime.jsx
@@ -1,5 +1,18 @@
+import { useEffect } from "react";
+import { useState } from "react";
+
 let CurrentTime = () => {
-  let time = new Date();
+  const [time, setTime] = useState(new Date());
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setTime(new Date());
+    }, 1000);
+
+    return () => {
+      clearInterval(intervalId);
+    };
+  }, []);
 
   return (
     <p className="lead">
