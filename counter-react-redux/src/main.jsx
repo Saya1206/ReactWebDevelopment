47341de5c24b7bdd35e18f370c3f7d7c58e99346
Saya1206 <sayali.patil0831>
2025-05-05 09:42:12 +0530

    setup React app entry with Redux provider

--- counter-react-redux/src/main.jsx
+++ counter-react-redux/src/main.jsx
@@ -2,9 +2,13 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import "bootstrap/dist/css/bootstrap.min.css";
+import { Provider } from "react-redux";
+import counterStore from "./store/index.js";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
+    <Provider store={counterStore}>
+      <App />
+    </Provider>
   </StrictMode>
 );
