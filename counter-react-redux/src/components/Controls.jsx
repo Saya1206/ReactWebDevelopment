c7c683251ff903d6b086e59884109e26dfe00c9a
Saya1206 <sayali.patil0831>
2025-05-05 23:45:12 +0530

    add input-based add and subtract features with proper input handling

--- counter-react-redux/src/components/Controls.jsx
+++ counter-react-redux/src/components/Controls.jsx
@@ -1,7 +1,11 @@
+import { useRef } from "react";
+import "../App.css";
+
 import { useDispatch } from "react-redux";
 
 const Controls = () => {
   const dispatch = useDispatch();
+  const inputElement = useRef();
 
   const handleIncrement = () => {
     dispatch({ type: "INCREMENT" });
@@ -11,23 +15,63 @@ const Controls = () => {
     dispatch({ type: "DECREMENT" });
   };
 
+  const handleAdd = () => {
+    dispatch({
+      type: "ADD",
+      payload: {
+        num: Number(inputElement.current.value),
+      },
+    });
+    inputElement.current.value = "";
+  };
+
+  const handleSubtract = () => {
+    dispatch({
+      type: "SUBTRACT",
+      payload: {
+        num: Number(inputElement.current.value),
+      },
+    });
+    inputElement.current.value = "";
+  };
+
   return (
-    <div className="d-grid gap-2 d-sm-flex justify-content-sm-center">
-      <button
-        type="button"
-        className="btn btn-primary "
-        onClick={handleIncrement}
-      >
-        +1
-      </button>
-      <button
-        type="button"
-        className="btn btn-success"
-        onClick={handleDecrement}
-      >
-        -1
-      </button>
-    </div>
+    <>
+      <div className="d-grid gap-2 d-sm-flex justify-content-sm-center">
+        <button
+          type="button"
+          className="btn btn-primary "
+          onClick={handleIncrement}
+        >
+          +1
+        </button>
+        <button
+          type="button"
+          className="btn btn-success"
+          onClick={handleDecrement}
+        >
+          -1
+        </button>
+      </div>
+      <div className="d-grid gap-2 d-sm-flex justify-content-sm-center control-row">
+        <input
+          type="text"
+          className="input-box"
+          placeholder="Enter Number"
+          ref={inputElement}
+        />
+        <button type="button" className="btn btn-warning" onClick={handleAdd}>
+          Add
+        </button>
+        <button
+          type="button"
+          className="btn btn-danger"
+          onClick={handleSubtract}
+        >
+          Subtract
+        </button>
+      </div>
+    </>
   );
 };
 
